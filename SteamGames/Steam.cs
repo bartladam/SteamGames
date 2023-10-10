3aca1fdbaea100b810e5f060bfe6e40057f333de
Adam46072 <adam.bartl2001@seznam.cz>
2023-10-10 19:00:36 +0200

    Steam got login and AddUser method

--- SteamGames/Steam.cs
+++ SteamGames/Steam.cs
@@ -8,5 +8,25 @@ namespace SteamGames
 {
     internal class Steam
     {
+        private List<User> users { get; set; }
+        public string loged { get; private set; }
+        public Steam()
+        {
+            users = new List<User>();
+        }
+        public void LogIn(string username, string password)
+        {
+            var user = from i in users
+                       where (i.username.Equals(username) && i.password.Equals(password))
+                       select new { Loged = "Loged: " + i.username };
+            foreach (var item in user)
+            {
+                loged = item.Loged;
+            }
+        }
+        public void AddUserToDatabase(User user)
+        {
+            users.Add(user);
+        }
     }
 }
