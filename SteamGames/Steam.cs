f433d3b4da8073490183e0fa911a5490d06ee601
Adam46072 <adam.bartl2001@seznam.cz>
2023-10-11 12:22:51 +0200

    Method BuyGame almost done

--- SteamGames/Steam.cs
+++ SteamGames/Steam.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,9 +11,12 @@ namespace SteamGames
     {
         private List<User> users { get; set; }
         public string loged { get; private set; }
-        public Steam()
+        public Library library { get; private set; }
+        public List<Games> listGames { get; private set; }
+        public Steam(params Games[] games)
         {
             users = new List<User>();
+            listGames = new List<Games>(games);
         }
         public void LogIn(string username, string password)
         {
@@ -27,6 +31,28 @@ namespace SteamGames
         public void AddUserToDatabase(User user)
         {
             users.Add(user);
+            library = new Library();
+        }
+        private void BuyGame(string nameGame)
+        {
+            foreach (Games item in listGames)
+            {
+                if(item.nameGame.Equals(nameGame))
+                {
+                    if(item.priceGame > 0)
+                    {
+                        PaymentGate p = new PaymentGate();
+                    }
+                    else
+                    {
+                        DownloadGame(item);
+                    }
+                }
+            }
+        }
+        private void DownloadGame(Games games)
+        {
+
         }
     }
 }
