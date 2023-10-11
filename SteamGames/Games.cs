123c86b3a90a74aaa563ef4741fd223c973b3e21
Adam46072 <adam.bartl2001@seznam.cz>
2023-10-11 12:09:50 +0200

    games class complete

--- SteamGames/Games.cs
+++ SteamGames/Games.cs
@@ -8,5 +8,15 @@ namespace SteamGames
 {
     internal class Games
     {
+        public string nameGame { get; init; }
+        public int priceGame { get; private set; }
+        public enum Genre {Arcade, Strategy, Simulator, Fantasy, Battle}
+        public Genre genre { get; init; }
+        public Games(string nameGame, int priceGame, Genre genre)
+        {
+            this.nameGame = nameGame;
+            this.priceGame = priceGame;
+            this.genre = genre;
+        }
     }
 }
