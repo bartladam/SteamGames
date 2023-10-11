f4f83849390930229c243bb11c4536edacde2220
Adam46072 <adam.bartl2001@seznam.cz>
2023-10-11 12:35:53 +0200

    payment gate complete

--- SteamGames/PaymentGate.cs
+++ SteamGames/PaymentGate.cs
@@ -8,5 +8,18 @@ namespace SteamGames
 {
     internal class PaymentGate
     {
+        public bool Pay(int priceGame)
+        {
+            int result = 0;
+            do
+            {
+                Console.WriteLine("Entry money:");
+                while(!int.TryParse(Console.ReadLine(), out result))
+                {
+                    Console.WriteLine("Entry money:");
+                }
+            } while (result >= priceGame);
+            return true;
+        }
     }
 }
